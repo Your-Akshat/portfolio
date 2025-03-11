f1569bc422febebe1148c72afa7564427745a315
Your-Akshat <officially11akshat@gmail.com>
2025-03-12 01:10:49 +0530

    initial setup

---
+++ src/pages/Home/components/About/About.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const About = () => {
+    return (
+        <div className="relative w-full h-screen py-10" id="about">
+            <h1 className="text-center w-full text-6xl font-extrabold text-blue-400">About Me</h1>
+        </div>
+    )
+}
+
+export default About;
