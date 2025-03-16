031b0f3465ef7bb1475fc5f44adefabec2ca47be
Your-Akshat <officially11akshat@gmail.com>
2025-03-16 14:55:33 +0530

    ui improvement

--- src/pages/Home/components/About/About.jsx
+++ src/pages/Home/components/About/About.jsx
@@ -3,10 +3,10 @@ import pic from "../../../../assets/pic.png"
 
 const About = () => {
     return (
-        <div className="relative w-full h-screen py-16" id="about">
-            <h1 className="text-center w-full text-6xl font-extrabold text-teal-600">About Me🚀</h1>
+        <div className="relative w-full h-screen py-24" id="about">
+            <h1 className="text-center w-full text-6xl font-extrabold text-teal-400">About Me🚀</h1>
 
-            <div className="flex justify-evenly items-center px-12 py-10">
+            <div className="flex justify-evenly items-center px-20 py-12 text-yellow-200">
 
                 <div className="w-1/3" >
                     <img src={pic} alt="imageElement" className="rounded-3xl w-96" />
@@ -14,20 +14,20 @@ const About = () => {
 
                 <div className="w-2/3" >
                     <div>
-                        <span className="font-semibold text-4xl">What Drives Me?</span>
+                        <span className="font-bold text-4xl">What Drives Me <span className="text-5xl">?</span></span>
                     
                         <ul className="pt-4 text-lg">
-                            <li className="pb-4">✨<b>Creative Development</b> – I enjoy crafting modern web applications with interactive and engaging features.</li>
-                            <li className="pb-4">🌟<b>Problem-Solving Mindset</b> – Challenges excite me! I approach complex problems analytically, always seeking efficient solutions.</li>
-                            <li className="pb-4">📚<b>Lifelong Learning</b> – Whether it’s mastering React.js, exploring AI/ML (RNNs), or keeping up with emerging tech, I constantly expand my skill set.</li>
-                            <li className="pb-4">🏆<b>Academic Excellence</b> – I ranked in the top percentile of JEE Mains, showcasing my analytical and mathematical strengths (100/100 in CBSE Class 10 Math).</li>
+                            <li className="pb-4">✨<b> Creative Development</b> – I enjoy crafting modern web applications with interactive and engaging features.</li>
+                            <li className="pb-4">🌟<b> Problem-Solving Mindset</b> – Challenges excite me! I approach complex problems analytically, always seeking efficient solutions.</li>
+                            <li className="pb-4">📚<b> Lifelong Learning</b> – Whether it’s mastering React.js or diving deep into concepts of Data Structures and Algorithms, I constantly expand my skill set.</li>
+                            <li className="pb-4">🏆<b> Academic Excellence</b> – I ranked in the top percentile of JEE Mains, showcasing my analytical and mathematical strengths (100/100 in CBSE Class 10 Math).</li>
                         </ul>
                     </div>
 
                     <div className="pt-8">
-                        <span className="font-semibold text-4xl mt-8">Beyond Tech</span>
+                        <span className="font-bold text-4xl mt-8">Beyond Tech</span>
                         
-                        <p className="pt-4 text-lg">✍I’m enthusiastic about literature and actively involved in poetry. I have participated in events like <b>Avlekha</b> and have had my poems published in the monthly magazine <i>Poem Nazm Aur Hum</i>, blending creativity with expression.</p>
+                        <p className="pt-4 text-lg">✍ I’m enthusiastic about literature and actively involved in poetry. I have participated in events like <b>Avlekha</b> and have had my poems published in the monthly magazine <i>Poem Nazm Aur Hum</i>, blending creativity with expression.</p>
                     </div>
 
                 </div>
