79ba60ebbb2694949d11af49aa363b91faae2905
Your-Akshat <officially11akshat@gmail.com>
2025-03-16 14:55:52 +0530

    remove excess code

--- src/pages/Home/components/Hero/Hero.jsx
+++ src/pages/Home/components/Hero/Hero.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import MyAvatar from "../../../../library/components/MyAvatar/MyAvatar";
 import GlowCircle from "../../../../library/components/GlowCircle/GlowCircle";
-import ResumeButton from "../../../../library/components/ResumeButton/ResumeButton";
-import { Link } from "react-router-dom";
 
 const Hero = () => {
   return (
@@ -19,15 +17,9 @@ const Hero = () => {
       </div>
 
       {/* Top Layer: Name */}
-      <h1 className="absolute z-20 text-[10rem] font-bold bottom-44 drop-shadow-[0_15px_15px_rgba(0,0,0)] text-center w-full pointer-events-none text-teal-500">AKSHAT JAIN</h1>
+      <h1 className="absolute z-20 text-[10rem] font-bold bottom-44 drop-shadow-[0_15px_15px_rgba(0,0,0)] text-center w-full pointer-events-none text-teal-300">AKSHAT JAIN</h1>
 
       <h1 className="z-20 text-3xl pointer-events-none w-full absolute text-yellow-500 font-bold bottom-48 text-center">. . . . . . . .  &nbsp; A PASSIONATE NAIVE</h1>
-      
-      {/* <div className="absolute w-full z-[20] bg-white bottom-52 font-bold left-[44rem]">
-        <Link to={"https://drive.google.com/file/d/1qFx_b4-rt9tVT_Otzj9PRlK3xPCOgWwx/view"} target="_blank">
-          <ResumeButton />
-        </Link>
-      </div> */}
 
     </div>
   );
