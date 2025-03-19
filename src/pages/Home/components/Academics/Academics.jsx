58250a0999051a473c44a19afbe8b8d8c03ef456
Your-Akshat <officially11akshat@gmail.com>
2025-03-19 21:41:48 +0530

    education details added

--- src/pages/Home/components/Academics/Academics.jsx
+++ src/pages/Home/components/Academics/Academics.jsx
@@ -1,9 +1,50 @@
 import React from "react";
+import Card from "../../../../library/commons/Card/Card";
 
 const Academics = () =>{
     return(
         <div id="academics" className="w-full py-24" >
-            <h1 className="text-center w-full text-6xl font-extrabold text-teal-400">Academics📈</h1>
+            <h1 className="text-center w-full text-6xl font-extrabold text-teal-400">Academics📚</h1>
+
+            <div className="pt-12 w-full flex items-center justify-center">
+                <ul className="timeline timeline-vertical">
+                    <li>
+                        <div className="timeline-start">
+                            <Card 
+                                width={"32rem"}
+                                title={"B.Tech - Electronics & Communication Engineering"}
+                                subtitle={"Indian Institute of Information Technology(IIIT) Kota"}
+                                body={["CGPA: 8.29"]}
+                            />
+                        </div>
+                        <hr className="bg-amber-500"/>
+                    </li>
+                    <li>
+                        <hr className="bg-amber-500"/>
+                        <div className="timeline-end">
+                            <Card 
+                                width={"32rem"}
+                                title={"Higher Secondary/Class-XII (CBSE)"}
+                                subtitle={"Chinar Public School, Alwar(Raj.)"}
+                                body={["Percentage: 92.6%"]}
+                            />
+                        </div>
+                        <hr className="bg-amber-500"/>
+                    </li>
+                    <li>
+                        <hr className="bg-amber-500" />
+                        <div className="timeline-start">
+                            <Card 
+                                width={"32rem"}
+                                title={"Secondary/Class-X (CBSE)"}
+                                subtitle={"Chinar Public School, Alwar(Raj.)"}
+                                body={["Percentage: 93.4%"]}
+                            />
+                        </div>
+                        <hr className="bg-amber-500"/>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
