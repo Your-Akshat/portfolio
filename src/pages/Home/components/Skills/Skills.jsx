7a3934f7983cb1801f443358d67392c89589841f
Your-Akshat <officially11akshat@gmail.com>
2025-03-20 22:39:59 +0530

    basic setup

---
+++ src/pages/Home/components/Skills/Skills.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Skills = () => {
+    return(
+        <div id="skills" className="w-full pt-24">
+            <h1 className="text-center w-full font-extrabold text-teal-400 text-6xl">Skills🧠</h1>
+        </div>
+    )
+}
+
+export default Skills;
