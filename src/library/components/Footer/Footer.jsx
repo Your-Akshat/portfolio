2879e756d8c4c2d3b7e08a6de1e23001733ea878
Your-Akshat <officially11akshat@gmail.com>
2025-03-31 15:44:29 +0530

    profile links and icons

--- src/library/components/Footer/Footer.jsx
+++ src/library/components/Footer/Footer.jsx
@@ -1,18 +1,38 @@
 import React from "react";
+import linkedin from "../../../assets/social_icons/linkedin.png"
+import github from "../../../assets/social_icons/github.png"
+import leetcode from "../../../assets/social_icons/leetcode.png"
+import mail from "../../../assets/social_icons/mail.png"
+import whatsapp from "../../../assets/social_icons/whatsapp.png"
 
 const Footer = () => {
     return(
-        <div className="w-full bottom-0 text-[#c9c9c9] text-center font-bold pt-6 pb-4 px-6 bg-[#212121]">
+        <div className="w-full bottom-0 text-[#c9c9c9] text-center font-bold pt-4 pb-4 px-6 bg-[#212121]">
             <div>
                 <h1 className="text-3xl">Profile Links</h1>
-                <div className="flex gap-12 justify-center flex-row items-center">
-                    <img src="" />
+
+                <div className="flex gap-12 justify-center flex-row items-center mt-8">
+                    <a href="https://www.linkedin.com/in/akshat-jain-69a751258/" target="_blank" rel="noreferrer noopener">
+                        <img src={linkedin} className="h-8" alt="Mail" />
+                    </a>
+                    <a href="https://github.com/Your-Akshat" target="_blank" rel="noreferrer noopener">
+                        <img src={github} className="h-8" alt="Mail" />
+                    </a>
+                    <a href="mailto:officially11akshat@gmail.com" target="_blank" rel="noreferrer noopener">
+                        <img src={mail} className="border border-white h-8 rounded-md" alt="Mail" />
+                    </a>
+                    <a href="https://leetcode.com/u/Your_Akshat/" target="_blank" rel="noreferrer noopener">
+                        <img src={leetcode} className="h-8" alt="Mail" />
+                    </a>
+                    <a href="https://wa.me/919664263934" target="_blank" rel="noreferrer noopener">
+                        <img src={whatsapp} className="h-8" alt="Mail" />
+                    </a>
                 </div>
             </div>
 
-            <hr className="h-[2px] bg-white my-3" />
+            <hr className="h-[2px] bg-white mt-6 mb-3" />
 
-            <span> &#169; Website designed and maintained by <a href="mailto:officially11akshat@gmail.com" target="_blank"> Akshat Jain </a> </span>
+            <span> &#169; Website designed and maintained by <a href="mailto:officially11akshat@gmail.com" target="_blank" rel="noreferrer noopener"> Akshat Jain </a> </span>
 
         </div>
     )
