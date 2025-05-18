1be99c56bfce11edebe6bc1784926123d233ee5d
Your-Akshat <officially11akshat@gmail.com>
2025-05-18 12:53:59 +0530

    about - all queries

--- src/pages/Home/components/About/About.jsx
+++ src/pages/Home/components/About/About.jsx
@@ -3,18 +3,18 @@ import pic from "../../../../assets/pic.png"
 
 const About = () => {
     return (
-        <div className="relative w-full h-screen py-24" id="about">
-            <h1 className="text-center w-full text-6xl font-extrabold text-teal-400">About Me🚀</h1>
+        <div className="relative w-full min-h-screen py-24" id="about">
+            <h1 className="text-center w-full text-5xl font-extrabold text-teal-400">About Me🚀</h1>
 
-            <div className="flex justify-evenly items-center px-20 py-12 text-yellow-200">
+            <div className="flex flex-col sm:flex-row sm:justify-evenly items-center sm:px-6 lg:px-20 py-12 text-yellow-200">
 
-                <div className="w-1/3" >
-                    <img src={pic} alt="imageElement" className="rounded-3xl w-96" />
+                <div className="sm:w-2/5 lg:1/3" >
+                    <img src={pic} alt="imageElement" className="rounded-3xl w-80 sm:w-[30rem]" />
                 </div>
 
-                <div className="w-2/3" >
+                <div className="w-[90%] sm:w-3/5 lg:w-2/3" >
                     <div>
-                        <span className="font-bold text-4xl">What Drives Me <span className="text-5xl">?</span></span>
+                        <span className="font-bold text-[2rem] sm:text-4xl">What Drives Me <span className="text-4xl sm:text-5xl">?</span></span>
                     
                         <ul className="pt-4 text-lg">
                             <li className="pb-4">✨<b> Creative Development</b> – I enjoy crafting modern web applications with interactive and engaging features.</li>
