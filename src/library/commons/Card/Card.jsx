d01751328d30f3408958532a354c61d49f85a48d
Your-Akshat <officially11akshat@gmail.com>
2025-05-19 13:50:42 +0530

    card - css update for better ui

--- src/library/commons/Card/Card.jsx
+++ src/library/commons/Card/Card.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Card = ({ title, subtitle, body, linkText, link }) => {
     return (
         <div className="w-full h-full">
-            <div className="card bg-amber-300 text-red-950 hover:bg-[#f5ca3c] hover:-translate-y-4 hover:shadow-2xl hover:shadow-[rgba(245,202,60,0.6)] w-full h-full flex flex-col">
+            <div className="card bg-amber-300 text-red-950 hover:bg-[#f5ca3c] hover:-translate-y-4 hover:shadow-2xl hover:shadow-[rgba(245,202,60,0.6)] w-full h-full">
                 <div className="card-body flex-grow">
                     <h2 className="text-center text-xl font-semibold">{title}</h2>
                     <h3 className="text-center text-lg font-semibold">{subtitle}</h3>
