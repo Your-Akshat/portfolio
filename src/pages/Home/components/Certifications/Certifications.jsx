076b6487f749b8fcb1e9822ddaf410c0fcb53b4e
Your-Akshat <officially11akshat@gmail.com>
2025-05-19 13:50:23 +0530

    cetificates - cards restructuring

--- src/pages/Home/components/Certifications/Certifications.jsx
+++ src/pages/Home/components/Certifications/Certifications.jsx
@@ -7,7 +7,7 @@ const Certifications = () => {
             <h1 className="w-full text-5xl sm:text-6xl text-center font-extrabold text-teal-400">Certifications and Achievements🏆</h1>
 
             <div className="pt-12 flex gap-8 justify-evenly items-center flex-wrap w-full">
-                <div className="w-[35rem]">
+                <div className="w-[35rem] h-fit sm:h-[14.5rem]">
                     <Card 
                         title={"The 2022 ICPC Asia Mathura Kanpur Regional Contest"}
                         subtitle={"Certificate of Achievement"}
@@ -17,17 +17,17 @@ const Certifications = () => {
                     />
                 </div>
 
-                <div  className="w-[35rem]">
+                <div  className="w-[35rem] h-fit sm:h-[14.5rem]">
                     <Card 
                         title={"Gen AI Mastermind by Outskill"}
                         subtitle={"Certificate of Completion"}
-                        body={["Completed a two-day Generative AI Mastermind conducted by Outskill, focusing on the application of AI tools for daily activities and an introduction to automation"]}
+                        body={["Completed a two-day Generative AI Mastermind conducted by Outskill, focusing on the application of AI tools for daily activities and an introduction to no-code automation"]}
                         linkText={"View Certificate"}
                         link={"https://drive.google.com/file/d/1ohw0i_RZXmxh5CE_QZVmZf4QCDdEMRBg/view?usp=drive_link"}
                     />
                 </div>
 
-                <div className="w-[35rem]">
+                <div className="w-[35rem] h-fit sm:h-[14.5rem]">
                     <Card 
                         title={"CBSE Secondary School Examination 2020"}
                         subtitle={"Certificate of Merit"}
@@ -37,11 +37,13 @@ const Certifications = () => {
                     />
                 </div>
 
-                <div className="w-[35rem] h-[14.5rem]">
+                <div className="w-[35rem] h-fit sm:h-[14.5rem]">
                     <Card 
                         title={"Avlekha, Stavya 2024"}
                         subtitle={"Winner - Poetry Competition"}
                         body={["Secured 1st place in the Poetry Competition 'Avlekha' held during Stavya 2024, the cultural fest at IIIT Vadodara"]}
+                        linkText={"View Post"}
+                        link={"https://www.instagram.com/p/C5iO7UJy1IY/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA%3D%3D"}
                     />
                 </div>
             </div>
