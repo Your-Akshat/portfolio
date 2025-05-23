2f285c2c75a9b89d246e85c5e8d4df847132d6fa
Your-Akshat <officially11akshat@gmail.com>
2025-05-24 00:09:46 +0530

    card update

--- src/library/commons/Card/Card.jsx
+++ src/library/commons/Card/Card.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const Card = ({ title, subtitle, body, linkText, link }) => {
     return (
         <div className="w-full h-full">
-            <div className="card bg-amber-300 text-red-950 hover:bg-[#f5ca3c] hover:-translate-y-4 hover:shadow-2xl hover:shadow-[rgba(245,202,60,0.6)] w-full h-full">
-                <div className="card-body flex-grow">
+            <div className="rounded-lg bg-amber-300 text-red-950 hover:bg-[#f5ca3c] hover:-translate-y-4 hover:shadow-2xl hover:shadow-[rgba(245,202,60,0.6)] w-full h-full">
+                <div className="p-6 flex-grow">
                     <h2 className="text-center text-xl font-semibold">{title}</h2>
                     <h3 className="text-center text-lg font-semibold">{subtitle}</h3>
                     {body.length === 1 ? (
