ccef681b81740e11840881f53f5efe5bd4854ba1
Your-Akshat <officially11akshat@gmail.com>
2025-05-23 22:23:59 +0530

    trail 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,40 @@
+// module.exports = {
+//   content: [
+//     "./src/**/*.{js,jsx,ts,tsx}",
+//     "./public/index.html",
+//   ],
+//   safelist: [
+//     "card",
+//     "card-body",
+//     "hover:-translate-y-4",
+//     "hover:shadow-2xl",
+//     "hover:bg-[#f5ca3c]",
+//     "hover:shadow-[rgba(245,202,60,0.6)]",
+//     "bg-amber-300",
+//     "text-red-950",
+//     "text-center",
+//     "text-blue-800",
+//     "text-sm",
+//     "underline",
+//     "list-disc",
+//     "list-inside",
+//     "font-semibold",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [require("daisyui")],
+// };
+
+
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./public/index.html",
-  ],
-  safelist: [
-    "card",
-    "card-body",
-    "hover:-translate-y-4",
-    "hover:shadow-2xl",
-    "hover:bg-[#f5ca3c]",
-    "hover:shadow-[rgba(245,202,60,0.6)]",
-    "bg-amber-300",
-    "text-red-950",
-    "text-center",
-    "text-blue-800",
-    "text-sm",
-    "underline",
-    "list-disc",
-    "list-inside",
-    "font-semibold",
+    './src/**/*.{js,jsx,ts,tsx}',
+    './public/index.html',
+    './safelist.txt', // Include the safelist file
   ],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [require('daisyui')],
 };
