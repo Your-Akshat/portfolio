610bf8507e2ad235082207b8e3c3993c6822c0d8
Your-Akshat <officially11akshat@gmail.com>
2025-05-23 22:19:24 +0530

    prod - trial 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,24 @@
-// module.exports = {
-//   content: [
-//     "./src/**/*.{js,jsx,ts,tsx}",
-//     "./public/index.html",
-//   ],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [require("daisyui")],
-// };
-
-
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
     "./public/index.html",
   ],
   safelist: [
-    "card", "card-body", "btn", "btn-primary", "bg-base-100", "shadow-xl",
-    "text-center", "text-red-950", "hover:-translate-y-4", "hover:shadow-2xl",
-    "hover:bg-[#f5ca3c]", "hover:shadow-[rgba(245,202,60,0.6)]"
+    "card",
+    "card-body",
+    "hover:-translate-y-4",
+    "hover:shadow-2xl",
+    "hover:bg-[#f5ca3c]",
+    "hover:shadow-[rgba(245,202,60,0.6)]",
+    "bg-amber-300",
+    "text-red-950",
+    "text-center",
+    "text-blue-800",
+    "text-sm",
+    "underline",
+    "list-disc",
+    "list-inside",
+    "font-semibold",
   ],
   theme: {
     extend: {},
