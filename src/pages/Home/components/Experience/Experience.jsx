19d2a788b5da973b6ac607fe4552330e170aa397
Your-Akshat <officially11akshat@gmail.com>
2025-11-10 17:21:45 +0530

    experience section - update

--- src/pages/Home/components/Experience/Experience.jsx
+++ src/pages/Home/components/Experience/Experience.jsx
@@ -6,22 +6,25 @@ const Experience = () =>{
 
     return(
         <div id="experience" className="w-full pt-24 pb-16" >
-            <h1 className="text-center w-full text-5xl sm:text-6xl font-extrabold text-teal-400">Experience📈</h1>
+            <h1 className="text-center w-full text-5xl sm:text-6xl font-extrabold text-[#3d0008]">Experience📈</h1>
 
             <div className="pt-12 w-full flex items-center justify-center">
                 <div className="w-80 md:w-[45rem] lg:w-[60rem]">
                     <Card
-                    title={"Web Developer Intern"}
-                    subtitle={"@The Golden Era Of Jainism (Nov'24 - present)"}
+                    title={"Frontend Developer Intern & Volunteer"}
+                    subtitle={"@GEO Jainism (Nov'24 - present | Internship: Jun'25 - Jul'25)"}
                     body={["Collaborating with a team of professional developers to build the organization’s website from scratch, focusing on both the admin panel and user-facing features for a platform offering information on Jain temples, literature, blogs, and history.", 
                     "Designing and implementing admin panel pages, integrating APIs, and enhancing functionality for both the admin panel and user-facing website, improving overall performance and user experience.",
-                    "Developing responsive, user-friendly web pages using React.js, JavaScript, SCSS, and MUI, ensuring seamless cross-device functionality and intuitive navigation."]} 
+                    "Developing responsive, user-friendly web pages using React.js, JavaScript, SCSS, and MUI, ensuring seamless cross-device functionality and intuitive navigation.",
+                    "Implemented Firebase Authentication integration in the admin panel having Role-Based Access Control (RBAC) and Attribute-Based Access Control (ABAC), enhancing secure data access and user permission management."]} 
+                    linkText={"Visit the website"}
+                    link={"https://www.geojainism.com"}
                     />
                 </div>
             </div>
 
             <div className="w-full pt-16 flex justify-center items-center">
-                <ResumeButton link={"https://drive.google.com/file/d/1qFx_b4-rt9tVT_Otzj9PRlK3xPCOgWwx/view"} />
+                <ResumeButton link={"https://drive.google.com/file/d/1n1aDj4ELF1a-751c6OfWBcuttB2v6syX/view?usp=sharing"} />
             </div>
 
         </div>
